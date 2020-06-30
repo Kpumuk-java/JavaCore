6c3ee3f35319a2f136cd8a74307d73679077cabb
Slava <bag001@yandex.ru>
2020-06-30 18:04:13 +0300

    update Lesson1.java

--- JavaCore (1 level)/Lesson1.java
+++ JavaCore (1 level)/Lesson1.java
@@ -3,10 +3,9 @@ package ru.geekbrains.lesson1;
 public class Lesson1 {
 
     public static void main(String[] args) {
-        //dataType();
-        //System.out.println("Возвращаемое значение " + complexCalculations());
-        //twoNumbers(10, 5);
-        methodDetermineYear ();
+
+        
+
     }
 
     public static void dataType () {
@@ -19,57 +18,51 @@ public class Lesson1 {
         char typeChar = '\uffff';
         boolean typeBoolean = true;
 
-        System.out.println("Тип данных  - [byte]     Значение переменной - [ " + typeByte + " ]");
+       /* System.out.println("Тип данных  - [byte]     Значение переменной - [ " + typeByte + " ]");
         System.out.println("Тип данных  - [short]    Значение переменной - [ " + typeShort + " ]");
         System.out.println("Тип данных  - [int]      Значение переменной - [ " + typeInt + " ]");
         System.out.println("Тип данных  - [long]     Значение переменной - [ " + typeLong + " ]");
         System.out.println("Тип данных  - [float]    Значение переменной - [ " + typeFloat + " ]");
         System.out.println("Тип данных  - [double]   Значение переменной - [ " + typeDouble + " ]");
         System.out.println("Тип данных  - [char]     Значение переменной - [ " + typeChar + " ]");
-        System.out.println("Тип данных  - [boolean]  Значение переменной - [ " + typeBoolean + " ]");
+        System.out.println("Тип данных  - [boolean]  Значение переменной - [ " + typeBoolean + " ]");*/
 
     }
 
-    public static float complexCalculations () {
-        float a = 0.1f, b = 11f, c = 10f, d = 10f;
+    public static float getComplexCalculations (float a, float b, float c , float d) {
         System.out.println("a = " + a + "   b = " + b + "   c = " + c + "   d = " + d);
         //System.out.println("Возвращаемое значение " + (a * (b + (c / d))));
         return a * (b + (c / d));
     }
 
-    public static boolean twoNumbers (int one_number, int two_number) {
+    public static boolean getTwoNumbers (int one_number, int two_number) {
         int sum = one_number + two_number;
-        if (sum >= 10 && sum <= 20) {
-            System.out.println("Сумма двух чисел находится в диапозоне от 10 до 20 включительно");
-            return true;
-        } else {
-            System.out.println("Сумма двух чисел находится за пределом диапозона от 10 до 20 включительно");
-            return false;
-        }
+        return sum >= 10 && sum <= 20;
     }
 
-    public static void determinePositiveOrNegativeNumber (int number) {
-        if (number == 0 || number > 0) {
+    public static void isPositiveOrNegativeNumber (int number) {
+        if (number >= 0) {
             System.out.println("Число является положительным");}
             else System.out.println("Число является отрицательным");
     }
 
-    public static boolean methodReturnTrueWhenNumberNegative (int number) {
-        if (number < 0) return true;
-        return false;
+    public static boolean getReturnTrueWhenNumberNegative (int number) {
+        return number < 0;
     }
 
-    public static void methodHello (String name) {
+    public static void isMethodHello (String name) {
         System.out.println("Привет, " + name);
     }
 
-    public static void methodDetermineYear () {
-        int year = 1199;
+    public static void isMethodDetermineYear (int year) {
 
-        if (year % 4 == 0)
+        if (year % 4 == 0)  {
             if (!(year % 100 == 0) || (year % 400 == 0)) System.out.println ("Год является высокосным");
-             else System.out.println("Год не является высокосным");
-        else System.out.println("Год не является высокосным");
+            else System.out.println("Год не является высокосным");
+            }
+        else {
+            System.out.println("Год не является высокосным");
+        }
     }
 
 }
