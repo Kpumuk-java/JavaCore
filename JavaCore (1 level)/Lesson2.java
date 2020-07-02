3647d9d256df30be01548c180ef3ff93321b6172
Slava <bag001@yandex.ru>
2020-07-02 23:42:29 +0300

    update Lesson 2.java + sortMassive

--- JavaCore (1 level)/Lesson2.java
+++ JavaCore (1 level)/Lesson2.java
@@ -2,18 +2,17 @@ package ru.geekbrains.lesson2;
 
 public class Lesson2 {
 
+
+
     public static void main(String[] args) {
-        int[] MIN_MAX_MASSIVE = {1, 2, 4, 5, 6};
-        getMinNumber(MIN_MAX_MASSIVE);
-        getMaxNumber(MIN_MAX_MASSIVE);
+
     }
 
-    public static boolean getCheckMassive (int [] massive) {
-        if (massive.length > 1) {
-            return true;
+    public static void isMinAndMaxMassive (int[] massive) {
+        if (massive.length > 0) {
+            int numberMax = getMaxNumber(massive);
+            int numberMin = getMinNumber(massive);
         }
-
-        return false;
     }
 
 
@@ -38,7 +37,7 @@ public class Lesson2 {
     }
 
     public static boolean getCheckBalance (int[] massive) {
-        if (getCheckMassive(massive)) {
+        if (massive.length > 1) {
             for (int i = 0; i < massive.length; i++) {
                 if (getLeftSumMassive(massive, i) == getRightSumMassive(massive, i)) {
                     return true;
@@ -78,11 +77,11 @@ public class Lesson2 {
     }
 
     public static void isDriftMassive (int[] massive, int n) {
-        if (getCheckMassive(massive)) {
+        if ((massive.length > 1) && !(n == 0)) {
             int countDrift;
 
             if (n < 0) {
-                countDrift = massive.length - (n % massive.length);
+                countDrift = massive.length + (n % massive.length);
             } else if (n > massive.length){
                 countDrift = n % massive.length;
             } else {
@@ -111,5 +110,70 @@ public class Lesson2 {
             }
         }
     }
+
+    public static void isMassiveZeroAndOne () {
+        int[] massive = {1, 0, 0, 0, 1, 1};
+
+        for (int i = 0; i < massive.length; i++) {
+            if (massive[i] == 0) {
+                massive[i] = 1;
+            } else {
+                if (massive[i] == 1) {
+                    massive[i] = 0;
+                }
+            }
+        }
+    }
+
+    public static void isMassiveX3 () {
+        int[] massive = new int[8];
+
+        for (int i = 0; i < massive.length; i++) {
+            massive[i] = i * 3;
+        }
+    }
+
+    public static void isMassiveX2ForNumberLessSix () {
+        int[] massive = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        for (int i = 0; i < massive.length; i++) {
+
+            if (massive[i] < 6) {
+                massive[i] = massive[i] * 2;
+            }
+
+        }
+    }
+
+    public static void isMassiveSquare (int[][] massive) {
+
+        for (int i = 0; i < massive.length ; i++) {
+            massive[i][i] = 1;
+            massive[massive.length - 1 - i][i] = 1;
+        }
+    }
+
+    public static void isSortMassive (int[] massive) {
+
+        if (massive.length > 1) {
+            int buffer, countBreak;
+
+            for (int i = 0; i < massive.length; i++) {
+                countBreak = 0;
+                if (!(i == 0) && (countBreak == 0)) {
+                    break;
+                }
+                for (int j = 0; j < massive.length - 1 - i; j++) {
+                    if (massive[j] > massive[j + 1]) {
+                        buffer = massive[j + 1];
+                        massive[j + 1] = massive[j];
+                        massive[j] = buffer;
+                        countBreak = 1;
+                    }
+                }
+            }
+        }
+    }
+
 
 }
