84440a02f56988f4f8a2c662c97cedb370e38c2c
Slava <bag001@yandex.ru>
2020-08-16 17:51:42 +0300

    update lesson5 (16.08.2020)

--- src/main/java/java2/lesson5/Main.java
+++ src/main/java/java2/lesson5/Main.java
@@ -4,10 +4,5 @@ public class Main {
 
 
     public static void main(String[] args) {
-        MyThreadsRun myThreadsRun = new MyThreadsRun();
-        myThreadsRun.oneThread();
-        //myThreadsRun.getValueMassiveTenNumber();
-        myThreadsRun.massiveFillOne();
-        myThreadsRun.twoThread();
     }
 }
--- src/main/java/java2/lesson5/MyThreadsRun.java
+++ src/main/java/java2/lesson5/MyThreadsRun.java
@@ -6,58 +6,42 @@ public class MyThreadsRun {
     private static final int size = 1000000;
     private static final int h = size / 2;
     private float[] arr = new float[size];
-    private static long buffer = 0;
-
-    public MyThreadsRun() {
-        Arrays.fill(arr, 1);
-    }
 
     /**
      * В каждую ячейку массив arr записывается значение по формуле в одном потоке
      */
     public void oneThread() {
+        initMassive();
         long a = System.currentTimeMillis();
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
+        massiveFillToForm(arr);
         System.out.println("\nВремя работы заполнения массива в одном потоке: " + (System.currentTimeMillis() - a));
     }
 
     /**
-     * В массив arr в каждую ячейку записывается значенеи по формуле в двух потоках через разбития
-     * массива на 2 массива и последующее склеивание
+     * В массив arr в каждую ячейку записывается значения по заданой формуле в двух потоках через разбития
+     * массива на 2 массива и последующее склеивание. Вывод на консоль время выполнения данного метода
      */
     public void twoThread() {
-        Long a = System.currentTimeMillis();
+        initMassive();
+        long a = System.currentTimeMillis();
 
-        Thread thread1 = new Thread((Runnable) () -> {
+        Thread thread2 = new Thread(() -> {
             float[] bufferArr = new float[h];
             System.arraycopy(arr, 0, bufferArr, 0, h);
-            bufferArr = massiveFillToForm(bufferArr);
+            massiveFillToForm(bufferArr);
             System.arraycopy(bufferArr, 0, arr, 0, h);
-            if (buffer < System.currentTimeMillis() - a) {
-                buffer = System.currentTimeMillis() - a;
-            }
         });
 
-        Thread thread2 = new Thread((Runnable) () -> {
-            float[] bufferArr = new float[h];
-            System.arraycopy(arr, h, bufferArr, 0, h);
-            bufferArr = massiveFillToForm(bufferArr);
-            System.arraycopy(bufferArr, 0, arr, h, h);
-            if (buffer < System.currentTimeMillis() - a) {
-                buffer = System.currentTimeMillis() - a;
-            }
-        });
-
-        thread1.start();
         thread2.start();
 
+        float[] bufferArr = new float[h];
+        System.arraycopy(arr, h, bufferArr, 0, h);
+        massiveFillToForm(bufferArr);
+        System.arraycopy(bufferArr, 0, arr, h, h);
+
         while (true) {
-            if (!thread1.isAlive() && !thread2.isAlive()) {
-                System.out.println("\nВремя работы заполнения массива в двух потоках: " + buffer);
-                //getValueMassiveTenNumber();
+            if (!thread2.isAlive()) {
+                System.out.println("\nВремя работы заполнения массива в двух потоках: " + (System.currentTimeMillis() - a));
                 break;
             }
         }
@@ -67,7 +51,7 @@ public class MyThreadsRun {
      * @param arr массив float
      * @return возвращает массив со значениями высчитанные по заданной формуле
      */
-    public float[] massiveFillToForm(float[] arr) {
+    private float[] massiveFillToForm(float[] arr) {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
@@ -75,18 +59,9 @@ public class MyThreadsRun {
     }
 
     /**
-     * Выводит на консоль первые и последние 10 ячеек массива arr
-     */
-    public void getValueMassiveTenNumber() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println(arr[i] + "   " + arr[size - i - 1]);
-        }
-    }
-
-    /**
      * Заполняет массив arr единицами
      */
-    public void massiveFillOne() {
+    private void initMassive () {
         Arrays.fill(arr, 1);
     }
 }
