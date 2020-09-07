994cb62cd5a56533f2b6a30c1652a5b0bc8c6bb3
Slava <bag001@yandex.ru>
2020-09-08 01:58:02 +0300

    finished java3.lesson3

--- JavaFX/src/sample/User.java
+++ JavaFX/src/sample/User.java
@@ -1,19 +1,22 @@
 package sample;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class User {
     private final String SERVER_ADDR = "localhost";
     private final int SERVER_PORT = 5151;
+    private Integer COUNT_HISTORY = 100;
 
     private Socket socket;
     private DataInputStream dis;
     private DataOutputStream dos;
     private boolean authorized = false;
     private boolean connected = false;
+
+    private File history = new File("history.txt");
 
 
     Controller controller;
@@ -28,13 +31,18 @@ public class User {
 
             while (true) {
                 String printServer = dis.readUTF();
-                if(printServer.startsWith("/authOk")) {
+                if (printServer.startsWith("/authOk")) {
                     authorized = true;
                     controller.chatField.appendText(printServer + "\n");
+                    if (history != null) {
+                        sendHistory();
+                    }
                     break;
                 }
+
                 controller.chatField.appendText(printServer + "\n");
             }
+
 
             while (!socket.isClosed()) {
                 String printServer = dis.readUTF();
@@ -44,6 +52,7 @@ public class User {
                         socket.close();
                         break;
                     }
+                    writeHistory(printServer);
                     controller.chatField.appendText(printServer + "\n");
                 }
             }
@@ -55,15 +64,61 @@ public class User {
         }
     }
 
-    public void sendMsg (String sendServer) {
-            try {
-                dos.writeUTF(sendServer);
-            } catch (IOException e) {
-                e.printStackTrace();
+    private void sendHistory() {
+        List<String> fullHistory = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(history))) {
+            String s = reader.readLine();
+            while (s != null) {
+                fullHistory.add(s);
+                s = reader.readLine();
             }
-            if (sendServer.equals("/exit")) {
-                closeConnected();
+
+            if (fullHistory.size() >= COUNT_HISTORY) {
+                for (int i = fullHistory.size() - COUNT_HISTORY; i < fullHistory.size(); i++) {
+                    controller.chatField.appendText(fullHistory.get(i) + "\n");
+                }
+            } else {
+                for (int i = 0; i < fullHistory.size(); i++) {
+                    controller.chatField.appendText(fullHistory.get(i) + "\n");
+                }
             }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * (У меня 3 полувопроса:
+     * 1. меня очень сильно смущает что приходится при каждой записи строки
+     * в историю открывать поток и закрывать поток записи в файл, это же сильно "тормозит программу", потери во времени
+     * 2. Сначало хотел сделать запись сразу в цикле while (!socket.isClosed()) где ждем данных с сервера,
+     * но (как я понимаю из-за того что запись в файл это фоновый поток) у меня банально строка не успевала записаться
+     * в файл txt, и обнулялась до следующей порции данных с сервера(((
+     * 3. Была ли необходимость хранить историю в байтах в файле data? (кроме того, что в таком ввиде вес файла минимален)
+     * )
+     * @param str строка которую нужно записать в историю
+     */
+    public void writeHistory (String str) {
+        try (FileWriter file = new FileWriter(history, true);) {
+            file.write(str + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(String sendServer) {
+        try  {
+            dos.writeUTF(sendServer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (sendServer.equals("/exit")) {
+            closeConnected();
+        }
 
     }
 
@@ -78,7 +133,7 @@ public class User {
         }
     }
 
-    public void setController (Controller con) {
+    public void setController(Controller con) {
         this.controller = con;
     }
 
